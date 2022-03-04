8732d807dbb7368614ffbc097a8cfb20c0d4fcc4
NotSoRed1 <red1doesarts@gmail.com>
2022-03-05 00:33:24 +0100

    .

--- app/main.py
+++ app/main.py
@@ -27,7 +27,7 @@ app = FastAPI()
 app.add_middleware(DBSessionMiddleware, db_url=settings.DATABASE_URL.replace("postgres://", "postgresql://"))
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
